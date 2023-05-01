92a1579ab723613b3f2cb5f1d422cbbc5f774111
Michael Ehab <michaelehab16@gmail.com>
2023-05-01 23:14:19 +0300

    consistent functions naming

--- Phase 2/logFiles.h
+++ Phase 2/logFiles.h
@@ -1,11 +1,11 @@
 FILE *memoryLogFile;
 FILE *schedulerLogFile;
 
-void addMemoryAllocation(int time, int bytes, int processId, int from, int to){
+void logMemoryAllocation(int time, int bytes, int processId, int from, int to){
     fprintf(memoryLogFile, "At time %d allocated %d bytes for process %d from %d to %d\n", time, bytes, processId, from, to);
 }
 
-void addMemoryDeallocation(int time, int bytes, int processId, int from, int to){
+void logMemoryDeallocation(int time, int bytes, int processId, int from, int to){
     fprintf(memoryLogFile, "At time %d freed %d bytes for process %d from %d to %d\n", time, bytes, processId, from, to);
 }
 
